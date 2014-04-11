6a68cbfa946bd62f0f49e69ef5a2491691f07115
Andreas Prodromou <prodromou.andreas@gmail.com>
2014-04-11 11:42:35 -0700

    Added a MindMap file in the organizer folder to keep track of all my work during the phd

---
+++ organizer/MindMap/phd_mindmap.mm
@@ -0,0 +1,120 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#ff0000" CREATED="1397161911493" ID="ID_1868557972" MODIFIED="1397188008000" TEXT="PhD UCSD 2013-??">
+<edge STYLE="sharp_bezier"/>
+<icon BUILTIN="gohome"/>
+<node CREATED="1397162009313" ID="ID_1968811078" MODIFIED="1397188008000" POSITION="right" TEXT="Dead Code Elimination">
+<edge STYLE="sharp_bezier"/>
+<node CREATED="1397177316151" ID="ID_243438931" MODIFIED="1397188008000" TEXT="Source Code">
+<edge STYLE="sharp_bezier"/>
+<node CREATED="1397177321336" ID="ID_750768643" MODIFIED="1397188064088" TEXT="Write Analyzer Tool">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      TODO:
+    </p>
+    <p>
+      ---------
+    </p>
+    <p>
+      Modify mem/packet.cc to stop showing warnings about discontiguous addresses (Takes a lot of output space) -- The code works so I might just leave it as is
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge STYLE="sharp_bezier"/>
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node CREATED="1397178359137" ID="ID_1353001224" MODIFIED="1397188007999" TEXT="Implement optimization mechanism">
+<edge STYLE="sharp_bezier"/>
+</node>
+</node>
+<node CREATED="1397177359832" ID="ID_1948826404" LINK="aprodrom@matricks.ucsd.edu:~/spek2k6_simpoints/checkpoints" MODIFIED="1397188007999" TEXT="Simpoints">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      The folder &quot;spek2k6_simpoints&quot; at matricks includes all the work for generating checkpoints, along with scripts that automate the procedure.
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge STYLE="sharp_bezier"/>
+<node CREATED="1397177367928" ID="ID_1090816176" LINK="aprodrom@matricks.ucsd.edu:spek2k6_simpoints/" MODIFIED="1397188007999" TEXT="Simpoint Profiling">
+<edge STYLE="sharp_bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="go"/>
+</node>
+<node CREATED="1397177375960" ID="ID_558161854" LINK="aprodrom@matricks.ucsd.edu:~/spek2k6_simpoints/run_simpoint.sh" MODIFIED="1397188007998" TEXT="Run Simpoint Tool">
+<edge STYLE="sharp_bezier"/>
+<icon BUILTIN="go"/>
+</node>
+<node CREATED="1397177397912" ID="ID_45704271" LINK="aprodrom@matricks.ucsd.edu:~/spek2k6_simpoints/create_checkpoints.sh" MODIFIED="1397188007998" TEXT="Create Checkpoints">
+<edge STYLE="sharp_bezier"/>
+<icon BUILTIN="go"/>
+</node>
+</node>
+<node CREATED="1397162022881" ID="ID_1329512717" MODIFIED="1397188007997" TEXT="Experiments">
+<edge STYLE="sharp_bezier"/>
+<node CREATED="1397177623542" ID="ID_1189086345" MODIFIED="1397188007997" TEXT="Dead instruction count">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Experiment Folder (matricks): /home/aprodrom/experiments/DeadCodeElimination/exp_2014_06_04
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge STYLE="sharp_bezier"/>
+<icon BUILTIN="go"/>
+</node>
+<node CREATED="1397177633398" ID="ID_814794738" MODIFIED="1397188007997" TEXT="Impact of inst. window size">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Experiment Folder (matricks): /home/aprodrom/experiments/DeadCodeElimination/exp_2014_10_04
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge STYLE="sharp_bezier"/>
+</node>
+</node>
+<node CREATED="1397177170777" ID="ID_1069340594" MODIFIED="1397188007997" TEXT="Paper">
+<edge STYLE="sharp_bezier"/>
+<node CREATED="1397178388801" ID="ID_1007667938" MODIFIED="1397188007997" TEXT="Title">
+<edge STYLE="sharp_bezier"/>
+<node CREATED="1397178421217" ID="ID_1511905915" MODIFIED="1397188007997" TEXT="Revisiting Dead Code Elimination">
+<edge STYLE="sharp_bezier"/>
+</node>
+<node CREATED="1397178427968" ID="ID_1336320260" MODIFIED="1397188007996" TEXT="Eliminating Dead Code under Big Data workloads">
+<edge STYLE="sharp_bezier"/>
+</node>
+</node>
+<node CREATED="1397178394513" ID="ID_939962512" MODIFIED="1397188007996" TEXT="Related Work">
+<edge STYLE="sharp_bezier"/>
+</node>
+<node CREATED="1397178400017" ID="ID_1994178629" MODIFIED="1397188007996" TEXT="Motivation">
+<edge STYLE="sharp_bezier"/>
+</node>
+<node CREATED="1397178410129" ID="ID_1242220949" MODIFIED="1397188007996" TEXT="Mechanism Implementation">
+<edge STYLE="sharp_bezier"/>
+</node>
+<node CREATED="1397178406305" ID="ID_1221585127" MODIFIED="1397188007996" TEXT="Results">
+<edge STYLE="sharp_bezier"/>
+</node>
+</node>
+</node>
+</node>
+</map>
